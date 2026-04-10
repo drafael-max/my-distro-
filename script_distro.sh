15e5ff9c26178675977dce0bc91b94436b92ad0f
drafael-max <drafaelpatin@gmail.com>
2026-04-10 15:39:56 +0000

    Execution of the long command to install the necessary dependencies

--- script_distro.sh
+++ script_distro.sh
@@ -1,3 +1,15 @@
 #This command updates the package list in the repositories to ensure that we install the latest versions of the tools.
 sudo apt update
 
+#What is each package for?
+gcc, make #To compile the kernel and BusyBox
+libncurses-dev #For the settings menus
+flex, bison, bc #Required by the kernel build
+cpio #To create the initramfs file system
+libelf-dev, libssl-dev #Kernel security dependencies
+syslinux #The boot manager
+dosfstools #To create the FAT file system
+qemu-system-x86 #To test the image without real hardware
+
+sudo apt install -y git vim make gcc libncurses-dev flex bison bc \
+cpio libelf-dev libssl-dev syslinux dosfstools qemu-system-x86
